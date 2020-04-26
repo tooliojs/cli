3f0d2eabcd79a8b877e1aafa6bd1784f54a93f73
arakilian0 <arakilian0@gmail.com>
2020-04-25 20:17:19 -0700

    ADDED acidjs/cli files - not configured yet!

--- bin/toolio.js
+++ bin/toolio.js
@@ -1,3 +1,22 @@
 #!/usr/bin/env node
-const config = require('@toolio/config')
-console.log(config)
+try {
+    const config = require('@toolio/config')
+    if(config) {
+        const chalk = require('chalk')
+        const yargs = require('yargs')
+        const PLUGINS = require('../lib/plugins')
+        const COMMANDS = require('../lib/commands')
+        const OPTIONS = require('../lib/options')
+        const { version } = require('../package.json')
+        yargs.usage('$0 <cmd> [args]')
+        if(config.cli.color === 'none') { yargs.scriptName('toolio'); yargs.version(`Toolio CLI: v${version}`) } 
+        else { yargs.scriptName(chalk[config.cli.color]('toolio')); yargs.version(chalk[config.cli.color](`Toolio CLI: v${version}`)) }
+        if(PLUGINS.length) PLUGINS.forEach(plugin => { yargs.command(...plugin) })
+        if(COMMANDS.length) COMMANDS.forEach(command => { yargs.command(...command) })
+        if(OPTIONS.length) OPTIONS.forEach(option => { yargs.option(...option) }) 
+        yargs.strictCommands(true)
+        yargs.demandCommand(1, 'You need at least one command before moving on.')
+        yargs.help(config.cli.help)
+        yargs.argv
+    } else return process.exit()
+} catch(err) { console.error(new Error(err)) }

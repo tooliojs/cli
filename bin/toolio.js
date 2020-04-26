b576f508a13407a6eed80c132912abe4481f5024
arakilian0 <arakilian0@gmail.com>
2020-04-26 10:35:40 -0700

    UPDATE cli script - checks for 'package.json'

--- bin/toolio.js
+++ bin/toolio.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 try {
+    const path = require('path')
+    const pkg = require(path.resolve('package.json'))
     const config = require('@toolio/config')
     if(config) {
         const chalk = require('chalk')
@@ -14,9 +16,10 @@ try {
         if(PLUGINS.length) PLUGINS.forEach(plugin => { yargs.command(...plugin) })
         if(COMMANDS.length) COMMANDS.forEach(command => { yargs.command(...command) })
         if(OPTIONS.length) OPTIONS.forEach(option => { yargs.option(...option) }) 
-        // yargs.strictCommands(true)
-        // yargs.demandCommand(1, 'You need at least one command before moving on.')
-        yargs.help(config.cli.help)
+        yargs.strictCommands(false)
+        yargs.recommendCommands(true)
+        yargs.demandCommand(1, 'You need at least one command before moving on.')
+        if(config.cli && config.cli.help !== undefined) yargs.help(config.cli.help)
         yargs.argv
-    } else { console.log('error: no cli configs detected'); process.exit() }
-} catch(err) { console.error(new Error(err)); process.exit() }
+    } else { console.log('error: no toolio:cli configs detected'); process.exit() }
+} catch(err) { console.error('error: directory doesn\'t seem to be a node program - no "package.json" file found'); process.exit() }
